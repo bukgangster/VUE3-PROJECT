afd0ce8e64eb62734e114a1717d0a3236c319ef3
LIM <limyangchae@kakao.com>
2024-12-27 12:40:09 +0900

    작업반영

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  proxy: {
-    "/lang": {
-      target: "http://43.203.214.173:5300", // HTTP 백엔드 주소
-      changeOrigin: true,
-      pathRewrite: { "^/lang": "" },
-    },
-  },
 });
